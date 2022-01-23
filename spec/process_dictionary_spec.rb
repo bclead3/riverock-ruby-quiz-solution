ea5d42f92c5b04e74aa42b811e1eda97fec98f9a
Ben Leadholm <bclead3@yahoo.com>
2022-01-23 15:19:11 -0600

    add additional spec

--- spec/process_dictionary_spec.rb
+++ spec/process_dictionary_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe 'ProcessDictionary' do
       expect(subject.file_path).to eq('spec/data/words')
     end
   end
+
+  describe 'initialize file_path and .gz file' do
+    let(:subject) { ProcessDictionary.new('words.tar.gz') }
+
+    it 'recognizes .gz file' do
+      expect(subject.file_path).to eq('words.tar.gz')
+      file = File.open(subject.file_path)
+      expect(subject.file.path).to eq(file.path)
+    end
+  end
 end
